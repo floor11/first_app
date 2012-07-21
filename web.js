d837b91a3363ef17e66cbdebbdc1067dbcf49e41
mathieu.humbert <mathieu.humbert@accenture.com>
2012-07-22 00:38:13 +0200

    init postgresql

--- web.js
+++ web.js
@@ -1,12 +1,25 @@
 var express = require('express');
+var pg = require('pg');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
+
+  pg.connect(process.env.DATABASE_URL, function(err, client) {
+  var query = client.query('SELECT * FROM question');
+
+  query.on('row', function(row) {
+    response.send(JSON.stringify(row));
+  });
+});
+  
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+
