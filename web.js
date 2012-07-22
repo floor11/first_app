fc165f6277dbb57fd8b2ffc66b18d46c208da07e
mathieu.humbert <mathieu.humbert@accenture.com>
2012-07-22 08:37:11 +0200

    connection to heroku postgres

--- web.js
+++ web.js
@@ -3,10 +3,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var conString = "postgres://cslbrviesfgsub:PUxL6311IxYvOYveJad-UQabj8@ec2-23-21-85-197.compute-1.amazonaws.com:5432/d8cdt1ga62ttei";
+
 app.get('/', function(request, response) {
   response.send('Hello World!');
 
-  pg.connect(process.env.DATABASE_URL, function(err, client) {
+  pg.connect(conString, function(err, client) {
   var query = client.query('SELECT * FROM question');
 
   query.on('row', function(row) {
